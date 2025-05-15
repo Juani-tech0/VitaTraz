d5e5e58482f00af57a18489d9b7378c523dfa5d5
VictorSanchezMula <victorsanchezmula@gmail.com>
2025-05-15 20:33:59 +0200

    Updating dependencies, libraries and other stuff for authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fl_vitatraz_app/theme/app_theme.dart';
 import 'package:fl_vitatraz_app/screens/screens.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
